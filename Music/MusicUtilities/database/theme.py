6db157c7c7f654018ddeb677b81a35f8040b5447
a r i <91373874+zigaz23@users.noreply.github.com>
2022-01-18 13:03:12 +0700

    Update theme.py

--- Music/MusicUtilities/database/theme.py
+++ Music/MusicUtilities/database/theme.py
@@ -1,13 +1,15 @@
 from typing import Dict, Union, List
+
 from Music import db
 
 notesdb = db.notes
 
 async def _get_theme(chat_id: int) -> Dict[str, int]:
-    _notes = await notesdb.find_one({"chat_id": chat_id})
+    _notes = await themedb.find_one({"chat_id": chat_id})
     if not _notes:
         return {}
     return _notes["notes"]
+
 
 async def get_theme(chat_id: int, name: str) -> Union[bool, dict]:
     name = name.lower().strip()
@@ -22,6 +24,6 @@ async def save_theme(chat_id: int, name: str, note: dict):
     name = name.lower().strip()
     _notes = await _get_theme(chat_id)
     _notes[name] = note
-    await notesdb.update_one(
+    await themedb.update_one(
         {"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True
     )
